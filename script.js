549ed987fe63c2dea64f2d4c1fa48a120c69cdd7
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2023-02-15 09:58:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function calcular() {
+
+    var p = document.getElementById('v-inicial').value
+    var i = document.getElementById('taxa').value
+    var t = document.getElementById('periodo').value
+    var a = document.getElementById('v-final')
+
+    var taxa = i/100
+
+    var montante = p * Math.pow((1 + taxa), t)
+    var tot = montante.toString();
+    $('#v-final').val(tot)
+}
